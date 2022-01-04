f85b2af0814f74108f8e92d4fc39bc4bcd382fdf
MRDJsekhar <saisekhar77492@gmail.com>
2022-01-04 13:22:07 +0530

    ptm added

--- src/components/masterdatamanagement/parameterThresholdMaster/ParameterThresholdpage.js
+++ src/components/masterdatamanagement/parameterThresholdMaster/ParameterThresholdpage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PTAppBar from './ptcChildComponents/PTAppbar';
+import PTMCard from './ptcChildComponents/PTMCard';
 
 export function ParameterThresholdpage() {
 	return (
 		<div>
 			<PTAppBar />
+			<PTMCard />
 		</div>
 	);
 }
