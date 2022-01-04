c5d404eb6fc2cca421a458097fef8532fb9bf291
MRDJsekhar <saisekhar77492@gmail.com>
2022-01-04 12:16:02 +0530

    updated

--- src/components/masterdatamanagement/parameterThresholdMaster/ParameterThresholdpage.js
+++ src/components/masterdatamanagement/parameterThresholdMaster/ParameterThresholdpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PTAppBar from './ptcChildComponents/PTAppbar';
 
-const ParameterThresholdpage = () => {
+export function  ParameterThresholdpage   () {
 	return (
 		<div>
 			<PTAppBar />
@@ -9,4 +9,4 @@ const ParameterThresholdpage = () => {
 	);
 };
 
-export default ParameterThresholdpage;
+
